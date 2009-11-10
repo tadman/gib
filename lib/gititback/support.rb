eb71e2a4f39268ca60ae92449b0cb9babd04c82b
Scott Tadman <scott@twg.ca>
2009-11-10 11:06:56 -0500

    Fix symbolize_hash_keys method

--- lib/gititback/support.rb
+++ lib/gititback/support.rb
@@ -2,6 +2,7 @@ class Gititback::Support
   def self.symbolize_hash_keys(hash)
     hash.inject({ }) do |h, (k, v)|
       h[k.to_sym] = v.is_a?(Hash) ? symbolize_hash_keys(v) : v
+      h
     end
   end
 end
